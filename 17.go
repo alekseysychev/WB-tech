f585d5839f16c2fd0b035464f776f7f6768102e8
Aleksey Sychev <79252045491@yandex.ru>
2021-06-29 16:47:54 +0300

    добавил reflect

--- 17.go
+++ 17.go
@@ -4,7 +4,10 @@ package main
 // определить тип переменной — int, string, bool, channel
 // из переменной типа interface{}.
 
-import "fmt"
+import (
+	"fmt"
+	"reflect"
+)
 
 func getType(v interface{}) {
 	switch v.(type) {
@@ -13,10 +16,29 @@ func getType(v interface{}) {
 		fmt.Println("string")
 	case int:
 		fmt.Println("int")
+	case int32:
+		fmt.Println("int32")
 	case bool:
 		fmt.Println("bool")
 	case chan struct{}:
 		fmt.Println("chan struct{}")
+	default:
+		fmt.Println("undefined")
+	}
+}
+
+func getType2(v interface{}) {
+	switch reflect.TypeOf(v).String() {
+	case "string":
+		fmt.Println("string")
+	case "int":
+		fmt.Println("int")
+	case "int32":
+		fmt.Println("int32")
+	case "bool":
+		fmt.Println("bool")
+	case "chan struct {}":
+		fmt.Println("chan struct {}")
 	default:
 		fmt.Println("undefined")
 	}
@@ -28,4 +50,12 @@ func main() {
 	getType('1')
 	getType(true)
 	getType(make(chan struct{}))
+
+	fmt.Println()
+
+	getType2("a")
+	getType2(1)
+	getType2('1')
+	getType2(true)
+	getType2(make(chan struct{}))
 }

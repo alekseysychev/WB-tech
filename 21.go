5b7aaa02e1d1577aed1980aa8fa69ab113634613
Aleksey Sychev <79252045491@yandex.ru>
2021-07-01 00:08:09 +0300

    21 переработано, добавлен вариант выполнения

--- 21.go
+++ 21.go
@@ -9,30 +9,55 @@ import (
 // Написать программу, которая в конкурентном виде читает элементы из массива в stdout.
 
 func main() {
-	a := []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
+	a := []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25}
 	current := 0
-	// mu := sync.Mutex{}
 	wg := sync.WaitGroup{}
 
-	reader := func(readerNumber int) {
-		defer wg.Done()
-		for {
-			// mu.Lock()
-			if len(a) <= current {
-				return
+	// простое чтение с блокировкой счётика текущего элемента
+	{
+		currentMutex := sync.RWMutex{}
+		reader := func(readerNumber int) {
+			defer wg.Done()
+			for {
+				currentMutex.Lock()
+				if len(a) <= current {
+					currentMutex.Unlock()
+					return
+				}
+				v := a[current]
+				fmt.Printf("%d(%d) ", v, readerNumber)
+				current++
+				currentMutex.Unlock()
+				time.Sleep(time.Millisecond * time.Duration(v))
 			}
-			v := a[current]
-			fmt.Printf("%d : %d\n", readerNumber, v)
-			current++
-			time.Sleep(time.Millisecond * time.Duration(v))
-			// mu.Unlock()
 		}
+
+		wg.Add(2)
+		go reader(1)
+		go reader(2)
+		wg.Wait()
+		fmt.Println()
 	}
+	// чтение через кусок слайса
+	{
+		reader := func(readerNumber int, sllice []int) {
+			defer wg.Done()
+			for _, v := range sllice {
+				fmt.Printf("%d(%d) ", v, readerNumber)
+			}
+		}
 
-	wg.Add(1)
-	go reader(1)
-	wg.Add(1)
-	go reader(2)
-
-	wg.Wait()
+		countReaders := 5
+		for i := 0; i < countReaders; i++ {
+			wg.Add(1)
+			start := len(a) / countReaders * i
+			stop := len(a) / countReaders * (i + 1)
+			if i == countReaders-1 {
+				stop = len(a)
+			}
+			go reader(i+1, a[start:stop])
+		}
+		wg.Wait()
+		fmt.Println()
+	}
 }

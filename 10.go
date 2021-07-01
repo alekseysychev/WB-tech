3766994cc8281954c02db22eb124063156099a4a
Aleksey Sychev <79252045491@yandex.ru>
2021-07-01 16:03:30 +0300

    10 фикс нуля

--- 10.go
+++ 10.go
@@ -11,10 +11,17 @@ import (
 
 func main() {
 	result := make(map[int][]float32)
-	t := []float32{-25.4, -27.0, 13.0, 19.0, 15.5, 24.5, -21.0, 32.5}
+	t := []float32{-9, 9, -25.4, -27.0, 13.0, 19.0, 15.5, 24.5, -21.0, 32.5}
 	for _, c := range t {
+		var index int
 		cInt := int(c)
-		index := cInt - cInt%10
+
+		if c < 0 {
+			index = cInt - cInt%10 - 10
+		} else {
+			index = cInt - cInt%10
+		}
+		fmt.Println(index, cInt, c)
 		result[index] = append(result[index], c)
 
 	}
